2a73cbdd133110befa9deb11e3d52aa7421da86b
AnhQuyet <anhquyet9943@gmail.com>
2021-01-25 09:43:04 +0700

    first commit

---
+++ lib/screens/details/details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_02/screens/details/components/body.dart';
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
